53234e7f1102dad35b625965025f2bfaec6986ec
chen.wu <zjax-005@163.com>
2013-01-08 11:35:39 +0800

    Add  color prompt for exceptional

--- python/autologin.py
+++ python/autologin.py
@@ -9,8 +9,31 @@ import urllib2
 import urllib
 import cookielib
 import json
+import os
 import datetime
 import time
+import ctypes
+
+STD_OUTPUT_HANDLE= -11
+FOREGROUND_BLUE = 0x01         # text color contains blue.  
+FOREGROUND_GREEN= 0x02         # text color contains green. 
+FOREGROUND_RED = 0x04          # text color contains red.
+FOREGROUND_INTENSITY = 0x08    # text color is intensified.  
+
+std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+
+def set_cmd_color(color, handle=std_out_handle):
+    """set the windows command console color."""
+    bool = ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color)  
+    return bool
+
+def reset_color():  
+    set_cmd_color(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
+
+def print_color_text(print_text):
+    set_cmd_color(FOREGROUND_RED |FOREGROUND_INTENSITY)
+    print print_text
+    reset_color()
 
 class Login_Oa():
     def __init__(self):
@@ -20,8 +43,8 @@ class Login_Oa():
         self.opener.addheaders = [("User-agent", "Mozilla/4.0 (compatible); MSIE 6.0; Windows NT 5.1")]
 
     def login(self, username, password):
-        url = "http://oa.shunwang.com/doLogin.do"         ## 可换成你想登录站点的url
-        data = {"username":username,"password":password}  
+        url = "http://oa.shunwang.com/doLogin.do"
+        data = {"username":username,"password":password}  ##登陆用户名和密码
         post_data = urllib.urlencode(data)
         req = urllib2.Request(url, post_data)
         try:
@@ -38,7 +61,7 @@ class Login_Oa():
         return True
 
     def logout(self):
-        url = "http://oa.shunwang.com/loginout.do"        ## 可换成你想退出站点的url
+        url = "http://oa.shunwang.com/loginout.do"
         req = urllib2.Request(url)
         html = self.opener.open(req)
         html.close()
@@ -56,14 +79,18 @@ class Login_Oa():
         for item in date_list:     ##将日志加上" 00:00:00"
             check_date.append(item + " 00:00:00")
         
-        url = "http://oa.shunwang.com/front/attendance/month.do?year="+ curr_year + "&month=" + curr_month + "&_=" + current_unix_time  ##查询当前年、月的考勤的url
+        url = "http://oa.shunwang.com/front/attendance/month.do?year="+ curr_year + "&month=" + curr_month + "&_=" + current_unix_time  ##查询当前年、月的考勤
         req = urllib2.Request(url)
         html = self.opener.open(req)
         checkpunch_js = json.loads(html.read())
-        valid_date = [item for item in check_date if item in checkpunch_js.keys()]   ##取得两个列表的交集        
-        for query_date in valid_date:        
-            print u"------------华丽丽的分割线------------"
-            print u"考勤日期: %s %s \n考勤人: %s\n考勤状态: %s\n\n第一次打卡时间: %d:%d\n早上打卡时间: %d:%d\n最后一次打卡时间: %d:%d\n下午打卡时间: %d:%d\n" % (query_date.split()[0],\
+        print check_date
+        valid_date = [item for item in check_date if item in checkpunch_js.keys()]   ##取得两个列表的交集
+        print valid_date
+        for query_date in valid_date:
+            print u"\n------------华丽丽的分割线------------"
+            if not checkpunch_js[query_date]["infos"]:
+                #print u"------------华丽丽的分割线------------"
+                print u"考勤日期: %s %s \n考勤人: %s\n考勤状态: %s\n\n第一次打卡时间: %s:%s\n早上打卡时间: %s:%s\n最后一次打卡时间: %s:%s\n下午打卡时间: %s:%s" % (query_date.split()[0],\
                                            checkpunch_js[query_date]["record"]["recordweekday"],\
                                            checkpunch_js[query_date]["record"]["realName"],\
                                            checkpunch_js[query_date]["record"]["attendanceStatus"],\
@@ -75,6 +102,20 @@ class Login_Oa():
                                            checkpunch_js[query_date]["record"]["lastPunch"]["minutes"],\
                                            checkpunch_js[query_date]["record"]["afternoonPunch"]["hours"],\
                                            checkpunch_js[query_date]["record"]["afternoonPunch"]["minutes"])
+            else:
+                if os.name == 'nt':
+                    print_color_text(u'！！！ 次奥~考勤有异常 ！！！') 
+                    print u"\n考勤日期: %s %s \n考勤人: %s\n\n异常原因:->" % (query_date.split()[0],\
+                                                                           checkpunch_js[query_date]["record"]["recordweekday"],\
+                                                                           checkpunch_js[query_date]["record"]["realName"]),
+                    print_color_text('%s' % checkpunch_js[query_date]["infos"][0]["reason"])
+
+                else:
+                    print u"\033[1;31;40m！！！次奥~考勤有异常！！！\033[0m\n考勤日期: %s %s \n考勤人: %s\n\n异常原因:-> %s" % (query_date.split()[0],\
+                                                                           checkpunch_js[query_date]["record"]["recordweekday"],\
+                                                                           checkpunch_js[query_date]["record"]["realName"],\
+                                                                           checkpunch_js[query_date]["infos"][0]["reason"])
+                    
         html.close()
            
 if __name__ == '__main__':
